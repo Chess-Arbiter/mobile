fc30c0b4021015a0ef1ad80267e2aa88bc57520e
Taron <taron.aleksanyan.20@gmail.com>
2022-12-28 19:32:24 +0400

    first commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+import { Provider as PaperProvider } from "react-native-paper";
+import theme from "./theme/theme";
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <PaperProvider theme={theme}>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </PaperProvider>
+    );
+  }
+}
---
+++ screens/HandbookScreen/HandbookScreen.tsx
@@ -0,0 +1,12 @@
+import { Card, Button } from "react-native-paper";
+
+export default function HandbookScreen() {
+  return (
+    <Card>
+      <Card.Actions>
+        <Button>Cancel</Button>
+        <Button>Ok</Button>
+      </Card.Actions>
+    </Card>
+  );
+}
