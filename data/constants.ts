963b0038264630261fdb6b72c60c01ccede02a33
Artyom Sukiasyan <artyom.s@sdc.am>
2023-01-29 20:26:12 +0400

    feat: change APP_BASE_URL

--- data/constants.ts
+++ data/constants.ts
@@ -1,6 +1,6 @@
 import { ELANGUAGES, ILANGUAGE } from "../models/global";
 
-export const APP_BASE_URL: string = "https://chess-arbiter-62925.web.app/";
+export const APP_BASE_URL: string = "https://chessarbiter.info/";
 export const LANGUAGES: ILANGUAGE[] = [
   { value: ELANGUAGES.EN, label: "English" },
   { value: ELANGUAGES.RU, label: "Русский" },
