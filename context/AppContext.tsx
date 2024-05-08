4ba6e476d708104475158b6588e55dab9e047996
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-05-08 19:21:47 +0400

    fix: fix context types

--- context/AppContext.tsx
+++ context/AppContext.tsx
@@ -1,13 +1,28 @@
 import React, { createContext, ReactNode, useContext } from "react";
+import { ColorSchemeName } from "react-native";
+import { WebViewErrorEvent } from "react-native-webview/lib/WebViewTypes";
+import { ELANGUAGES } from "../models/global";
 
-const AppContext = createContext<any>({});
+interface IContextData {
+  settings: {
+    lang: ELANGUAGES;
+    theme: ColorSchemeName;
+  };
+  colorScheme: "light" | "dark";
+  changeSettings: (key: string, value: string) => Promise<void> | undefined;
+  currentUrl: string;
+  onNavigationStateChange: (params: { url: string }) => void;
+  onError?: (event: WebViewErrorEvent) => void;
+}
+
+const AppContext = createContext<IContextData>({} as IContextData);
 
 const AppProvider = ({
   children,
   contextData,
 }: {
   children: ReactNode;
-  contextData: any;
+  contextData: IContextData;
 }) => {
   return (
     <AppContext.Provider value={contextData}>{children}</AppContext.Provider>
