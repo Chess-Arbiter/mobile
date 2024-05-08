ded3970251a7d81feb1adf6db1870f5e44cd1847
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-05-08 18:15:39 +0400

    feat: optimize calculateRating

--- util/helpers.ts
+++ util/helpers.ts
@@ -8,16 +8,13 @@ export function calculateRating({
   res,
 }: CalculatorSchemeType): CalculationResult {
   const k = k_value || 10;
-  let diff = o_r - y_r;
-  if (diff < -400) {
-    diff = -400;
-  } else if (diff > 400) {
-    diff = 400;
-  }
+  const diff = getRatingDiff({ y_r, o_r });
+
   const r = Math.pow(10, diff / 400);
   const E = 1 / (1 + r);
   const current_rating = y_r + k * (res - E);
   const change = -(y_r - current_rating);
+
   return {
     change: +Number(change).toFixed(2),
     current_rating: +Number(current_rating).toFixed(2),
@@ -26,4 +23,18 @@ export function calculateRating({
 
 export function formatFloatNumber(num: number): number {
   return Math.round(num * 100) / 100;
+}
+
+function getRatingDiff({ y_r, o_r }: { y_r: number; o_r: number }): number {
+  const diff = o_r - y_r;
+
+  if (diff < -400) {
+    return -400;
+  }
+
+  if (diff > 400) {
+    return 400;
+  }
+
+  return diff;
 }
