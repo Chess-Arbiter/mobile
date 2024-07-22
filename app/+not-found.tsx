56d4189f698f6eabb48449b8fe5381cff0a3d7f3
Artyom Sukiasyan <artiom.suqiasyan1401@gmail.com>
2024-07-22 12:22:21 +0400

    feat: update NotFound component

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,42 +1,46 @@
-import React from "react";
-import { StyleSheet, TouchableOpacity, View } from "react-native";
-import { Text } from "react-native-paper";
+import NotFoundScreen from "../screens/NotFoundScreen/NotFoundScreen";
 
-import { RootStackScreenProps } from "../types";
+export default NotFoundScreen
 
-export default function NotFoundScreen({
-  navigation,
-}: RootStackScreenProps<"NotFound">) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>This screen doesn't exist.</Text>
-      <TouchableOpacity
-        onPress={() => navigation.replace("Root")}
-        style={styles.link}
-      >
-        <Text style={styles.linkText}>Go to home screen!</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+// import React from "react";
+// import { StyleSheet, TouchableOpacity, View } from "react-native";
+// import { Text } from "react-native-paper";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: "#2e78b7",
-  },
-});
+// import { RootStackScreenProps } from "../types";
+
+// export default function NotFoundScreen({
+//   navigation,
+// }: RootStackScreenProps<"NotFound">) {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>This screen doesn't exist.</Text>
+//       <TouchableOpacity
+//         onPress={() => navigation.replace("Root")}
+//         style={styles.link}
+//       >
+//         <Text style={styles.linkText}>Go to home screen!</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     padding: 20,
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: "bold",
+//   },
+//   link: {
+//     marginTop: 15,
+//     paddingVertical: 15,
+//   },
+//   linkText: {
+//     fontSize: 14,
+//     color: "#2e78b7",
+//   },
+// });
